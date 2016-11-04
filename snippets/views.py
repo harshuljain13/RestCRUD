da5968d93466b5356e2cd212af553a2e75ab8e54
harshul1610 <harshulrobo@gmail.com>
2016-11-04 13:29:53 +0530

    added browsable api using rest framework request and response objects

--- snippets/views.py
+++ snippets/views.py
@@ -6,6 +6,12 @@ from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+#from rest_framework.request import Request
+
+
 # Create your views here.
 class JSONResponse(HttpResponse):
     """
@@ -16,43 +22,51 @@ class JSONResponse(HttpResponse):
     	kwargs['content_type'] = 'application/json'
     	super(JSONResponse, self).__init__(content,**kwargs)
 
-@csrf_exempt
-def snippet_list(request):
+@api_view(['GET','POST'])
+#@csrf_exempt
+def snippet_list(request,format=None):
     if request.method == 'GET':
     	snippets = Snippet.objects.all()
     	serializer = SnippetSerializer(snippets, many=True)
-    	return JSONResponse(serializer.data)
+    	#return JSONResponse(serializer.data)
+    	return Response(serializer.data)
 
     elif request.method == 'POST':
-    	data = JSONParser().parse(request)
-    	serializer = SnippetSerializer(data=data)
+    	#data = JSONParser().parse(request)
+    	serializer = SnippetSerializer(data=request.data)
     	if serializer.is_valid():
     		serializer.save()
-    		return JSONResponse(serializer.data,status=201)
-    	return JSONResponse(serializer.errors,status=440)
+    		#return JSONResponse(serializer.data,status=status.HTTP_201_CREATED)
+    		return Response(serializer.data,status=status.HTTP_201_CREATED)
+    	return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)
 
-@csrf_exempt
-def snippet_detail(request,pk):
+#@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
+def snippet_detail(request,pk,format=None):
 	"""
 	Retrieve, update & delete a code snippet
 	"""
 	try:
 		snippet = Snippet.objects.get(pk=pk)
 	except Snippet.DoesNotExist:
-		return HttpResponse(status=404)
+		#return HttpResponse(status=404)
+		return Response(status=status.HTTP_404_NOT_FOUND)
 
 	if request.method=='GET':
 		serializer = SnippetSerializer(snippet)
-		return JSONResponse(serializer.data)
+		return Response(serializer.data)
+
 	elif request.method == 'PUT':
-		data = JSONParser().parse(request)
-		serializer = SnippetSerializer(snippet, data=data)
+		#data = JSONParser().parse(request)
+		serializer = SnippetSerializer(snippet, data=request.data)
 		if serializer.is_valid():
 			serializer.save()
-			return JSONResponse(serializer.data)
-		return JSONResponse(serializer.errors,status=400)
+			return Response(serializer.data)
+		return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)
 
 	elif request.method == 'DELETE':
 		snippet.delete()
-		return HttpResponse(status=204)
+		#return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
 
