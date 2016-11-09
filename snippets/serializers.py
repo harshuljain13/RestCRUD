9fa581afaf093c4123d4d3381b8b3236973edec8
harshul1610 <harshulrobo@gmail.com>
2016-11-09 22:55:48 +0530

    understanding relations in rest api

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,19 +3,24 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 # User serializer class to add forward relationship between user and snippets.
-class UserSerializer(serializers.ModelSerializer):
-	snippets = 	serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+#class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+	#snippets = 	serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+	snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only = True)
 
 	class Meta:
 		model=User
 		fields = ('id','username','snippets')	
 
-class SnippetSerializer(serializers.ModelSerializer):
+#class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
 	# owner is added so as to relate it with owner in perform_create. also ass owner in fields
 	#so as to interact with the model.
 	owner  = serializers.ReadOnlyField(source='owner.username')
+	#making the relationship between snippet and highlight code
+	highlight = serializers.HyperlinkedIdentityField(view_name = 'snippet-highlight', format = 'html')
 
 	class Meta:
 		model = Snippet
-		fields = ('id','title', 'code', 'linenos', 'language' , 'style', 'owner')
+		fields = ('url','id','highlight','title', 'code', 'linenos', 'language' , 'style', 'owner')
 
--- snippets/urls.py
+++ snippets/urls.py
@@ -10,10 +10,12 @@ urlpatterns = [
     #url(r'^snippet/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     #url(r'^snippets/$',views.SnippetList1.as_view()),
     #url(r'^snippet/(?P<pk>[0-9]+)/$', views.SnippetDetail1.as_view()),
-    url(r'^snippets/$',views.SnippetList2.as_view()),
-    url(r'^snippet/(?P<pk>[0-9]+)/$', views.SnippetDetail2.as_view()),
-    url(r'^users/$',views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^snippets/$',views.SnippetList2.as_view(), name='snippet-list'),
+    url(r'^snippet/(?P<pk>[0-9]+)/$', views.SnippetDetail2.as_view(), name='snippet-detail'),
+    url(r'^users/$',views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+    url(r'^$', views.api_root),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
